26de53bebb98a2afa850d79eb270070f25b7cca7
Pairode Jaroensri <p.k.jaroensri@berkeley.edu>
2017-09-01 11:50:21 -0700

    updated description.

--- sorting/sleep_sort.py
+++ sorting/sleep_sort.py
@@ -1,5 +1,6 @@
 """
 Sort a list by putting the system to sleep for the duration of the list element itself.
+Algorithm as described on 4chan.
 Author: Pairode Jaroensri
 Last visited: May 15, 2017
 """
