4fc885615be8b5cc9a517a4c4bf644e611248f60
Pairode Jaroensri <p.k.jaroensri@berkeley.edu>
2018-02-28 03:05:06 -0800

    added documentation for plot_cpu

--- plot_cpu_temp.py
+++ plot_cpu_temp.py
@@ -1,6 +1,6 @@
 """
 Author: Pairode Jaroensri
-Last revision: Feb 28
+Last revision: Feb 28, 2018
 
 This program plots the CPU temperature in real time with poll rate of 1 Hz.
 Dependencies: lm-sensors, numpy, matplotlib
@@ -13,6 +13,7 @@ import matplotlib.pyplot as plt
 import matplotlib.ticker as ticker
 from matplotlib import animation
 
+# Set up the figures
 fig, ax = plt.subplots()
 ax.yaxis.tick_right()
 ax.yaxis.set_label_position("right")
@@ -24,15 +25,13 @@ ax.xaxis.set_major_locator(ticker.MultipleLocator(10))
 ax.xaxis.set_minor_locator(ticker.MultipleLocator(2))
 ax.yaxis.set_major_locator(ticker.MultipleLocator(10))
 ax.yaxis.set_minor_locator(ticker.MultipleLocator(2))
-
 for i in range(0, 10):
     plt.axhline(i*10, color='0.75')
 
-
-
-x = np.arange(-59, 1, 1)
+# Define history range and temperature polling interval
+x = np.arange(-60, 1, 1)
 temps = []
-for i in range(60):
+for i in range(61):
     temps.append(0)
 
 line, = ax.plot(x, temps)
@@ -41,17 +40,15 @@ line.set_color('r')
 def animate(i):
     global temps
     sensors = os.popen('sensors').read()
-    physical = float(sensors[349:353]) # Change the string slicing to match your system output
-    # core0 = float(sensors[409:413])
-    # core1 = float(sensors[469:473])
+    physical = float(sensors[349:353])
+    # Change the string slicing of the above line to match your system output
+    # The slicing should give you the desired sensor reading from lm-sensors
     print(physical, "°C")
     temps.append(physical)
     temps.pop(0)
     line.set_ydata(temps)  # update the data
     return line,
 
-
-# Init only required for blitting to give a clean slate.
 def init():
     line.set_ydata(np.zeros(60))
     return line,
