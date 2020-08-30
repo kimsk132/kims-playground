76a107a5930d63c9f1f55d1a543bf99c94c30fd9
Pairode Jaroensri <kim.gs.sk132@gmail.com>
2020-08-29 23:40:08 -0500

    employee scheduling

---
+++ employee_scheduling/readme.md
@@ -0,0 +1,18 @@
+## Employee Preference Table
+
+Please fill out the employee preference table with your own "preference" scheme. An example of such scheme is as follow:
+
+* 1 = The employee prefers this shift
+* 2 = The employee dislikes this shift
+* 3 = The employee strongly dislikes this shift
+* leave blank if the employee cannot make this shift
+
+Notice that bigger number means that the employee dislikes this shift.
+
+Feel free to change the row and column names, or add or delete rows and column to suit your need.
+
+Once you finish, please save the table as a *CSV* file with the name *_employee_preferences.csv_*
+
+## Known Limitations
+
+* Shifts cannot overlap. If you wish to have overlapping shifts, then you must manually add the constraints
---
+++ employee_scheduling/scheduling.py
@@ -0,0 +1,53 @@
+import cvxpy as cp
+import numpy as np
+import pandas as pd
+
+# Read in the preference table.
+preference_table = pd.read_csv('./employee_preferences.csv', index_col=0)
+# n = num employees
+# m = number of shifts
+n, m = preference_table.shape
+# Read in the shift requirements
+shift_requirements = pd.read_csv('./shift_requirements.csv', index_col=0)
+shift_numpy = shift_requirements.to_numpy()
+shift_people = shift_numpy[:,0]
+shift_hours = shift_numpy[:,1]
+# Read in employee requirements
+employee_requirements = pd.read_csv('./employee_requirements.csv', index_col=0)
+employee_hours = employee_requirements.to_numpy().reshape(n)
+# Deciding the "infeasibility" threshold.
+# The cost should be lower than threshold if every employee can work the shift they are assigned to.
+threshold = n * preference_table.max().max() * 10
+
+# Fill the blanks in preference_table with this threshold
+preference_table = preference_table.fillna(threshold)
+
+# Define the optimization problem
+X = cp.Variable((n, m), boolean=True)
+C = cp.Parameter((n, m))
+C.value = preference_table.to_numpy()
+
+# Add constrains here or comment out the ones you dont need
+constraints = [
+	# X @ shift_hours >= employee_hours, 	# Each employee must take a certain number of hours as defined in employee_requirements
+	np.ones(n) @ X >= shift_people 	# Each shift needs a certain number of employees as defined in shift_requirements
+]
+
+# First term reflects the employee's preferences, and the second term is trying to minimizes the number of hours each person works
+obj = cp.Minimize(cp.sum(cp.multiply(C, X)) + cp.sum_squares(X @ shift_hours - employee_hours))
+
+prob = cp.Problem(obj, constraints)
+prob.solve()
+
+assignment = pd.DataFrame(X.value,
+					index = preference_table.index,
+					columns = preference_table.columns)
+
+print("Status: ", prob.status)
+print("The optimal cost is", prob.value)
+print("A solution is")
+print(assignment)
+if prob.value >= threshold:
+	print("WARNING: Some employees are scheduled to work the shift they cannot make. Time to hire more people?")
+
+assignment.to_csv('./assignment.csv')
