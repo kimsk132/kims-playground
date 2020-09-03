23f022c6ad4225c0fe078ab83f2d40502536764e
Pairode Jaroensri <kim.gs.sk132@gmail.com>
2020-09-02 22:14:07 -0500

    Better input

--- employee_scheduling/readme.md
+++ employee_scheduling/readme.md
@@ -1,18 +1,23 @@
-## Employee Preference Table
+## Employee Scheduler
 
-Please fill out the employee preference table with your own "preference" scheme. An example of such scheme is as follow:
+This program is designed to read a csv output from Google Form responses along with *_shift_requirements.csv_*, and *_overlap.csv_*, then formulate the employee scheduling problem as an Integer Programming model. In order to use this program, you must supply:
+
+* form_responses.csv
+* overlap.csv
+* shift_requirements.csv
+* preference_scheme.json
+
+Each file's example are included with this repository.
+
+## preference_scheme.json
+
+This file defines your own "preference" scheme, which will be a part of Google Form answer. An example of such scheme is as follow:
 
 * 1 = The employee prefers this shift
-* 2 = The employee dislikes this shift
-* 3 = The employee strongly dislikes this shift
+* 2 = The employee is okay with this shift
+* 3 = The employee dislikes this shift
 * leave blank if the employee cannot make this shift
 
-Notice that bigger number means that the employee dislikes this shift.
+The bigger the number, the less likely that the employee will be assigned the shift.
 
-Feel free to change the row and column names, or add or delete rows and column to suit your need.
-
-Once you finish, please save the table as a *CSV* file with the name *_employee_preferences.csv_*
-
-## Known Limitations
-
-* Shifts cannot overlap. If you wish to have overlapping shifts, then you must manually add the constraints
+The program will read in this preference scheme, then replace verbal response in the form response with the corresponding number before feeding to the integer programming model.
--- employee_scheduling/scheduling.py
+++ employee_scheduling/scheduling.py
@@ -1,20 +1,31 @@
-import cvxpy as cp
 import numpy as np
 import pandas as pd
+import cvxpy as cp
+import json
 
-# Read in the preference table.
-preference_table = pd.read_csv('./employee_preferences.csv', index_col=0)
+# Read in form responses
+responses = pd.read_csv('form_responses.csv', index_col=2)
+responses.drop('Timestamp', axis=1, inplace=True)
+responses.drop('Email Address', axis=1, inplace=True)
+responses.drop('Privacy Policy', axis=1, inplace=True)
+responses.drop('Instruction', axis=1, inplace=True)
+
+# Read in how many hours per week each person needs to work
+employee_hours = responses['Hours per week'].to_numpy()
+
+# Generate preference_table
+preference_table = responses.copy()
+preference_table.drop('Hours per week', axis=1, inplace=True)
 # n = num employees
 # m = number of shifts
 n, m = preference_table.shape
-# Read in the shift requirements
-shift_requirements = pd.read_csv('./shift_requirements.csv', index_col=0)
-shift_numpy = shift_requirements.to_numpy()
-shift_people = shift_numpy[0]
-shift_hours = shift_numpy[1]
-# Read in employee requirements
-employee_requirements = pd.read_csv('./employee_requirements.csv', index_col=0)
-employee_hours = employee_requirements.to_numpy().reshape(n)
+
+# Replace preference words with weight value we defined in "preference_scheme.json"
+with open("preference_scheme.json") as f:
+    preference_scheme = json.load(f)
+
+for pref, weight in preference_scheme.items():
+    preference_table.replace(pref, weight, inplace=True)
 
 # Deciding the "infeasibility" threshold.
 # The cost should be lower than threshold if every employee can work the shift they are assigned to.
@@ -22,17 +33,26 @@ dislike_factor = preference_table.max().max()
 threshold = n * dislike_factor * 100
 
 # Fill the blanks in preference_table with this threshold
-preference_table = preference_table.fillna(threshold)
+preference_table.fillna(threshold, inplace=True)
+
+# TODO: Read in shift requirements
+shift_requirements = pd.read_csv('./shift_requirements.csv', index_col=0)
+shift_numpy = shift_requirements.to_numpy()
+shift_people = shift_numpy[0]
+shift_hours = shift_numpy[1]
+
+# Make sure the shift requirement is consistent with preference table
+assert all(shift_requirements.columns == preference_table.columns), "Shift requirement table must have the same columns as preference table."
 
 # Define the optimization problem
 X = cp.Variable((n, m), boolean=True)
 C = cp.Parameter((n, m))
 C.value = preference_table.to_numpy()
 
-# Add constrains here or comment out the ones you dont need
+# Add constrains here.
 constraints = [
-	# X @ shift_hours >= employee_hours, 	# Each employee must take a certain number of hours as defined in employee_requirements
-	np.ones(n) @ X >= shift_people 	# Each shift needs a certain number of employees as defined in shift_requirements
+    X @ shift_hours >= employee_hours,    # Each employee must take a certain number of hours as defined in employee_requirements
+    np.ones(n) @ X >= shift_people  # Each shift needs a certain number of employees as defined in shift_requirements
 ]
 
 # Handling overlapping shifts
@@ -54,14 +74,43 @@ prob = cp.Problem(obj, constraints)
 prob.solve(verbose=True)
 
 assignment = pd.DataFrame(X.value,
-					index = preference_table.index,
-					columns = preference_table.columns).round()
+                    index = preference_table.index,
+                    columns = preference_table.columns).round().astype('int')
 
 print("Status: ", prob.status)
 print("The optimal cost is", prob.value)
 print("A solution is")
 print(assignment)
-if prob.value >= threshold:
-	print("WARNING: Some employees are scheduled to work the shift they cannot make. Time to hire more people?")
+assignment.to_csv('./raw_output.csv')
 
-assignment.to_csv('./assignment.csv')
+# Format the output to be easy to read
+print("Result: ")
+output = {}
+for name, row in assignment.iterrows():
+    assigned = []
+    for shift, value in row:
+        if value == 1:
+            assigned.append(shift)
+    output[name] = assigned
+    print("{}: {}".format(name, assigned))
+
+with open('assignment.json', 'w') as f:
+    json.dump(output, f)
+
+
+# Check that everyone can make the shifts they are assigned
+assignment_cost = C.value * X.value
+if any(assignment_cost >= threshold):
+    print("WARNING: Some people are scheduled to work the shift they cannot make.")
+    for row_idx, row in enumerate(assignment_cost):
+        for col_idx, cost in enumerate(row):
+            if cost >= threshold:
+                print("{} cannot make {}".format(assignment.index[row_idx], assignment.columns[col_idx]))
+
+# Check if anyone is assigned more hours than they need to work
+number_hours = X.value @ shift_hours
+if any(number_hours >= employee_hours):
+    print("WARNING: Some people are assigned more hours than they need.")
+    for name_idx, hours in enumerate(number_hours):
+        if hours >= threshold:
+                print("{} is assigned {} hours, but needs {}".format(assignment.index[name_idx], number_hours[name_idx], employee_hours[name_idx]))
